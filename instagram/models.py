366f05d4d3c818aa52e012e245f03113aedaa461
Paulwamaria <paulwamaria@gmail.com>
2019-10-11 14:42:32 +0300

    Add classes, Profile, Image and Comment

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic =  models.ImageField(upload_to='media/', default='media/default.jpg')
+    bio = models.TextField()
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='media/', default='media/default.jpg')
+    image_name = models.CharField(max_length=60)
+    image_caption = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    likes = models.IntegerField(default=0)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.image_name
+
+
+class Comment(models.Model):
+    comments = models.TextField()
+    image =models.ForeignKey(Image, on_delete = models.CASCADE)
