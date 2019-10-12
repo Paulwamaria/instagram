d9716ab56aeaafdce56d97d78b02d4acd696609b
Paulwamaria <paulwamaria@gmail.com>
2019-10-12 08:10:01 +0300

    transfer the registration funtcion from instagram app to users app

---
+++ users/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render,redirect
+from .forms import InstaRegistrationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = InstaRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data['email']
+            full_name = form.cleaned_data['full_name']
+            username = form.cleaned_data.get('username')
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
+            messages.success(request, f'Account created for {username}')
+            return redirect('home')
+    else:
+        form = InstaRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
