eb059c70a9d92ad7cec346422bfc0f612b84e789
Paulwamaria <paulwamaria@gmail.com>
2019-10-13 09:21:49 +0300

    Add a condition controll statement to check if the update forms are valid and then save the data

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from .forms import InstaRegistrationForm, UserUpdateForm, ProfileUpdateFoem
+from .forms import InstaRegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 
 def register(request):
@@ -21,8 +21,18 @@ def register(request):
 
 @login_required
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateFoem()
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance = request.user)
+        
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance = request.user.profile)
+        if u_form.is_valid and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+    else:
+        u_form = UserUpdateForm(instance = request.user)
+        
+        p_form = ProfileUpdateForm(instance = request.user.profile)
+
 
     context={
         'u_form':u_form,
