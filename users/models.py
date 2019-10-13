ec09a00208883866f517fa34a92c9b7d32a56322
Paulwamaria <paulwamaria@gmail.com>
2019-10-13 09:44:50 +0300

    Add  a function to restrict the size of the image

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 
@@ -17,3 +18,14 @@ class Profile(models.Model):
 
     def delete_profile(self):
         self.delete()
+
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 500 or img.width > 500:
+            output_size = (500, 500)
+
+            img.thumbnail(output_size)
+            img.save(self.image.path)
