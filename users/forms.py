1f99a6222fd0085e37d82c85f06d9f202ebcbb4d
Paulwamaria <paulwamaria@gmail.com>
2019-10-13 08:59:02 +0300

    Add classes for User and Profile update forms

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class InstaRegistrationForm(UserCreationForm):
@@ -25,7 +26,7 @@ class InstaRegistrationForm(UserCreationForm):
         fields = ['email','full_name','username','password1','password2']
 
     def save(self, commit=True):
-        user =super(UserCreationForm, self).save(commit=False)
+        user =super(InstaRegistrationForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
         user.full_name = self.cleaned_data['full_name']
         user.username = self.cleaned_data['username']
@@ -37,3 +38,19 @@ class InstaRegistrationForm(UserCreationForm):
 
 
         return user
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required = True,label='',widget=forms.TextInput(attrs = {'class':'form-control','placeholder':'Email'}))
+    full_name = forms.CharField(max_length=60,required = True, label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Full Name'}))
+    username = forms.CharField(max_length=60,required = True, label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Username'}))
+
+    class Meta:
+        model = User
+        fields = ['email','full_name','username']
+
+
+class ProfileUpdateFoem(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_pic','bio']
