2c892c5afb9e8e9b4d6599a7a2da477dff055adc
Paulwamaria <paulwamaria@gmail.com>
2019-10-13 22:00:42 +0300

    Add a path to display a users specific posts

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ImageListView,ImageDetailView,ImageCreateView,ImageUpdateView,ImageDeleteView
+from .views import ImageListView,ImageDetailView,ImageCreateView,ImageUpdateView,ImageDeleteView,UserImageListView
 from . import views
 
 
 urlpatterns = [
     path('',ImageListView.as_view(), name = 'home'),
+    path('user/<str:username>/',UserImageListView.as_view(), name = 'user-images'),
     path('image/<int:pk>/',ImageDetailView.as_view(), name = 'image-detail'),
     path('image/new/',ImageCreateView.as_view(), name = 'image-create'),
     path('image/<int:pk>/update/',ImageUpdateView.as_view(), name = 'image-update'),
