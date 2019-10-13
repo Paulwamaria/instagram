693a9068c1228b68e5a2e0c79c51295024593e43
Paulwamaria <paulwamaria@gmail.com>
2019-10-13 14:29:09 +0300

    create a user instance  to enable display of the current user in sidebar

--- instagram/views.py
+++ instagram/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from .models import Image
 
 
 def home(request):
     message = "Welcome to Instagram"
 
+    images = Image.objects.all()
+    user = User.objects.get(username=request.user.username)
+
     context = {
-        "message":message
+        "message":message,
+        'images':images,
+        'user':user
     }
     return render(request, 'instagram/home.html',context)
 
