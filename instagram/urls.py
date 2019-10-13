b349a2601ff7f397983d382f8d9dc3e17b394881
Paulwamaria <paulwamaria@gmail.com>
2019-10-13 16:17:35 +0300

    Add paths for creating and displaying a new image

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from .views import ImageListView,ImageDetailView,ImageCreateView
 from . import views
 
 
 urlpatterns = [
-    path('',views.home, name = 'home'),
+    path('',ImageListView.as_view(), name = 'home'),
+    path('image/<int:pk>/',ImageDetailView.as_view(), name = 'image-detail'),
+    path('image/new/',ImageCreateView.as_view(), name = 'image-create'),
    
 ]
