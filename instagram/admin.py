2bb0c35f74a05cf4a9ee5473d6e109ae88cbed80
Paulwamaria <paulwamaria@gmail.com>
2019-10-15 16:32:16 +0300

    Register class followers

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Image, Comment
+from .models import Image, Comment,Followers
 
 # Register your models here.
 
 admin.site.register(Image)
 admin.site.register(Comment)
+admin.site.register(Followers)
